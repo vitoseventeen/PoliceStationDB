b5e546217dd6352a3c2374be94ce08714be04746
Vitalii Stepaniuk <stepavi2@fel.cvut.cz>
2024-05-23 00:34:21 +0200

    upravy

--- src/main/java/JPA/app/Main.java
+++ src/main/java/JPA/app/Main.java
@@ -16,26 +16,29 @@ public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("ApplicationPU");
         EntityManager em = emf.createEntityManager();
-
-
-
         // OSOBA
-
         OsobaDao osobaDao = new OsobaDao();
         osobaDao.setEntityManager(em);
-        osobaDao.create("5555555551", "M", 34, LocalDate.ofEpochDay(1990-11-11), "Walker", "John");
-        osobaDao.create("33333333331", "M", 25,LocalDate.ofEpochDay(1999-3-11) ,"Walker", "John");
+        try {
+            em.getTransaction().begin();
+            osobaDao.create("5555555551", "M", 34, LocalDate.ofEpochDay(1990 - 11 - 11), "Walker", "John");
+            osobaDao.create("33333333331", "M", 25, LocalDate.ofEpochDay(1999 - 3 - 11), "Walker", "John");
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println("Osoba with the specified data already exists.");
+        }
         List<Osoba> osoby = osobaDao.findAllBySurnameAndName("Walker", "John");
         for (Osoba osoba : osoby) {
             System.out.println(osoba.getPrijmeni() + " " + osoba.getJmeno());
         }
-       try {
-                em.getTransaction().begin();
-                osobaDao.updateNameByBiometrickeUdaje("555555555", "newName");
-                em.getTransaction().commit();
-            } catch (Exception e) {
-                System.out.println("No osoba found with the specified data.");
-            }
+
+        try {
+            em.getTransaction().begin();
+            osobaDao.updateNameByBiometrickeUdaje("555555555", "newName");
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println("No osoba found with the specified data.");
+        }
         try {
             em.getTransaction().begin();
             osobaDao.deleteBySurnameAndName("Walker", "John");
@@ -50,8 +53,14 @@ public class Main {
 
         PolicistaDao policistaDao = new PolicistaDao();
         policistaDao.setEntityManager(em);
-        policistaDao.create(new JPA.entities.Policista("4343432", "M", 34, LocalDate.ofEpochDay(1990-11-11), "Norton", "Joe", "K9", "Oddeleni_020" , 22 , null));
-        policistaDao.create(new JPA.entities.Policista("2121232", "M", 34, LocalDate.ofEpochDay(1990-11-11), "Tyson", "Mike", "Agent", "Oddeleni_013" , 44, null));
+        try {
+            em.getTransaction().begin();
+            policistaDao.create(new JPA.entities.Policista("4343432", "M", 34, LocalDate.ofEpochDay(1990 - 11 - 11), "Norton", "Joe", "K9", "Oddeleni_020", 22, null));
+            policistaDao.create(new JPA.entities.Policista("2121232", "M", 34, LocalDate.ofEpochDay(1990 - 11 - 11), "Tyson", "Mike", "Agent", "Oddeleni_013", 44, null));
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println("Policista with the specified data already exists.");
+        }
         try {
             em.getTransaction().begin();
             policistaDao.updateNameByBiometrickeUdaje("222222", "Bob");
@@ -83,12 +92,20 @@ public class Main {
 
         ZlocinecDao zlocinecDao = new ZlocinecDao();
         zlocinecDao.setEntityManager(em);
-        zlocinecDao.create("1111939", "F", 22, LocalDate.ofEpochDay(2002-12-12), "Hilfiger", "Anna", true, 165, 52);
-        zlocinecDao.create("2222299", "M", 33, LocalDate.ofEpochDay(1990-11-11), "Walker", "John", false, 180, 80);
+        try {
+            em.getTransaction().begin();
+            zlocinecDao.create("1111939", "F", 22, LocalDate.ofEpochDay(2002 - 12 - 12), "Hilfiger", "Anna", true, 165, 52);
+            zlocinecDao.create("2222299", "M", 33, LocalDate.ofEpochDay(1990 - 11 - 11), "Walker", "John", false, 180, 80);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println("Zlocinec with the specified data already exists.");
+        }
 
         zlocinecDao.findBySurnameAndName("Walker", "John");
 
         zlocinecDao.findBySurnameAndName("Hilfiger", "Anna");
+        em.getTransaction().commit();
+
 
         try {
             em.getTransaction().begin();
@@ -107,15 +124,11 @@ public class Main {
         }
 
 
-
-
         // ZLOCINEC END
-
 
 
         closeEntityManager(em, emf);
     }
-
 
 
     public static void closeEntityManager(EntityManager em, EntityManagerFactory emf) {
