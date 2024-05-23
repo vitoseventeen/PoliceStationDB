3fc41dfb24be1792fb8548056c1755196a0e07cf
Vitalii Stepaniuk <stepavi2@fel.cvut.cz>
2024-05-23 14:44:56 +0200

    finish v2

--- src/main/java/JPA/app/Main.java
+++ src/main/java/JPA/app/Main.java
@@ -16,73 +16,27 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class Main {
+
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("ApplicationPU");
+    private static EntityManager em = emf.createEntityManager();
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("ApplicationPU");
         EntityManager em = emf.createEntityManager();
-        OsobaDao osobaDao = new OsobaDao();
-        PolicistaDao policistaDao = new PolicistaDao();
-        ZlocinecDao zlocinecDao = new ZlocinecDao();
-
         try {
 
-            em.getTransaction().begin();
             // POLICISTA
-            policistaDao.setEntityManager(em);
-            policistaDao.create("904092190", "M", 34, LocalDate.of(1990, 11, 11), "Norton", "Joe", "FBI", "Oddeleni_020", 7, null);
-            List<Policista> policiste = policistaDao.findByBiomData("904092190");
-            for (Policista policista : policiste) {
-                System.out.println(policista.getPrijmeni() + " " + policista.getJmeno());
-            }
-            policistaDao.updateNameByBiometrickeUdaje("904092190", "Jack");
-            policistaDao.deleteByBiomData("904092190");
-
-            System.out.println("---------------------TRANZAKCE-----------------------------");
+            crudPolicista();
+            crudOsoba();
+            crudZlocinec();
+            tranzakceCP4();
 
             //tranzakce z CP4
-            try {
-                Query updateQuery = em.createQuery("UPDATE Policista p SET p.pocetLetSluzby = p.pocetLetSluzby + 1");
-                updateQuery.executeUpdate();
-                Query selectQuery = em.createQuery("SELECT p FROM Policista p WHERE p.specializace = :specializace");
-                selectQuery.setParameter("specializace", "Kriminalistika");
-                List<Policista> policisti = selectQuery.getResultList();
-                System.out.println("Policisti specializace Kriminalistika:");
-                System.out.println("----------------");
-                for (Policista policista : policisti) {
-                    System.out.println(policista.getPrijmeni() + " " + policista.getJmeno());
-                }
-                System.out.println("----------------");
-            } catch (Exception e) {
-                System.out.println("An error occurred: " + e.getMessage());
-                if (em.getTransaction().isActive()) {
-                    em.getTransaction().rollback();
-                }
-            }
-            //konec tranzakce
-
-            // OSOBA
-            System.out.println("---------------------OSOBA-----------------------------");
-            osobaDao.setEntityManager(em);
-            osobaDao.create("129049012", "M", 34, LocalDate.of(1990, 11, 11), "Franklin", "Ben");
-            List<Osoba> osoby = osobaDao.findByBiomData("129049012");
-            for (Osoba osoba : osoby) {
-                System.out.println(osoba.getPrijmeni() + " " + osoba.getJmeno());
-            }
-            osobaDao.updateNameByBiometrickeUdaje("129049012", "John");
-            osobaDao.deleteByBiomData("129049012");
 
 
-            System.out.println("---------------------ZLOCINEC-----------------------------");
-            // ZLOCINEC
-            zlocinecDao.setEntityManager(em);
-            zlocinecDao.create("9430289991", "M", 31, LocalDate.of(1993, 12, 12), "Hilfiger", "Tommy", true, 178, 74);
-            List<Zlocinec> zlocinci = zlocinecDao.findByBiomData("9430289991");
-            for (Zlocinec zlocinec : zlocinci) {
-                System.out.println(zlocinec.getPrijmeni() + " " + zlocinec.getJmeno());
-            }
-            zlocinecDao.updateNameByBiometrickeUdaje("9430289991", "Bobbie");
-            zlocinecDao.deleteByBiomData("9430289991");
+            // osoba
 
-            em.getTransaction().commit();
+            //zlocinec
+
 
             System.out.println("---------------------END-----------------------------");
         } catch (Exception e) {
@@ -105,5 +59,86 @@ public class Main {
     public static EntityManager getEntityManager() {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("ApplicationPU");
         return emf.createEntityManager();
+
+    }
+
+    public static void crudZlocinec() {
+
+        em.getTransaction().begin();
+
+        ZlocinecDao zlocinecDao = new ZlocinecDao();
+        System.out.println("---------------------ZLOCINEC-----------------------------");
+        // ZLOCINEC
+        zlocinecDao.setEntityManager(em);
+        zlocinecDao.create("9430289991", "M", 31, LocalDate.of(1993, 12, 12), "Hilfiger", "Tommy", true, 178, 74);
+        List<Zlocinec> zlocinci = zlocinecDao.findByBiomData("9430289991");
+        for (Zlocinec zlocinec : zlocinci) {
+            System.out.println(zlocinec.getPrijmeni() + " " + zlocinec.getJmeno());
+        }
+        zlocinecDao.updateNameByBiometrickeUdaje("9430289991", "Bobbie");
+        zlocinecDao.deleteByBiomData("9430289991");
+
+        em.getTransaction().commit();
+
+    }
+
+    public static void crudOsoba() {
+        em.getTransaction().begin();
+
+        OsobaDao osobaDao = new OsobaDao();
+        System.out.println("---------------------OSOBA-----------------------------");
+        osobaDao.setEntityManager(em);
+        osobaDao.create("129049012", "M", 34, LocalDate.of(1990, 11, 11), "Franklin", "Ben");
+        List<Osoba> osoby = osobaDao.findByBiomData("129049012");
+        for (Osoba osoba : osoby) {
+            System.out.println(osoba.getPrijmeni() + " " + osoba.getJmeno());
+        }
+        osobaDao.updateNameByBiometrickeUdaje("129049012", "John");
+        osobaDao.deleteByBiomData("129049012");
+
+        em.getTransaction().commit();
+
+    }
+
+    public static void tranzakceCP4() {
+
+        em.getTransaction().begin();
+
+        System.out.println("---------------------TRANZAKCE-----------------------------");
+        try {
+            Query updateQuery = em.createQuery("UPDATE Policista p SET p.pocetLetSluzby = p.pocetLetSluzby + 1");
+            updateQuery.executeUpdate();
+            Query selectQuery = em.createQuery("SELECT p FROM Policista p WHERE p.specializace = :specializace");
+            selectQuery.setParameter("specializace", "Kriminalistika");
+            List<Policista> policisti = selectQuery.getResultList();
+            System.out.println("Policisti specializace Kriminalistika:");
+            for (Policista policista : policisti) {
+                System.out.println(policista.getPrijmeni() + " " + policista.getJmeno());
+            }
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+        }
+
+        em.getTransaction().commit();
+    }
+
+    public static void crudPolicista() {
+        em.getTransaction().begin();
+
+        PolicistaDao policistaDao = new PolicistaDao();
+        policistaDao.setEntityManager(em);
+        policistaDao.create("31313141", "M", 34, LocalDate.of(1990, 11, 11), "Norton", "Joe", "FBI", "Oddeleni_020", 7, null);
+        List<Policista> policiste = policistaDao.findByBiomData("31313141");
+        for (Policista policista : policiste) {
+            System.out.println(policista.getPrijmeni() + " " + policista.getJmeno());
+        }
+        policistaDao.updateNameByBiometrickeUdaje("31313141", "Jack");
+        policistaDao.deleteByBiomData("31313141");
+
+        em.getTransaction().commit();
+
     }
 }
